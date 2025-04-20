84f4244505be928b2953d4e4ea59714e67c986d7
MrZVIL <zvz.ilya@gmail.com>
2025-04-20 15:03:39 +0300

    Add test of ObjReader

---
+++ test3.cpp
@@ -0,0 +1,39 @@
+#include "src/Image/BMPWriter.h"
+#include "src/Image/Image.h"
+#include "src/Object/ObjReader.h"
+#include "src/Object/Object.h"
+#include "src/Object/Triangle.h"
+#include "src/Renderer/Renderer.h"
+#include "src/Scene/Camera.h"
+#include "src/Scene/World.h"
+#include <chrono>
+#include <iostream>
+#include <string>
+
+using namespace renderer;
+
+void test_render() {
+  const char *path = "teapot.bmp";
+  std::string obj_path = "teapot.obj";
+  ObjReader objreader(obj_path);
+  renderer::Object obj = objreader.read();
+  // Object obj{{tr124}, {0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+  renderer::World world{};
+  world.add_new_object((renderer::Object)obj);
+  renderer::Camera camera{{0, 0, 3}, {0, 0, -1}, {0, 1, 0}, 1, 2, 2};
+  Renderer renderer(Width(512), Height(512));
+  Image img = renderer.render(world, camera);
+  auto start = std::chrono::high_resolution_clock::now();
+  int frames_count = 120;
+  for (int i = 0; i < frames_count; ++i) {
+    img = renderer.render(world, camera);
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::milli> duration = end - start;
+  std::cout << "Время отрисовки " << frames_count
+            << " кадров: " << duration.count() << " миллисекунд" << std::endl;
+  BmpWriter bmpwriter(path);
+  bmpwriter.write(img);
+}
+
+int main() { test_render(); }
